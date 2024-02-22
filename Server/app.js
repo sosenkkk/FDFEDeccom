9fa73c1b9ac21ae053292b073c18e20cac04f961
Sosenk <shashankraj3636@gmail.com>
2024-02-23 02:22:24 +0530

    morgan

--- Server/app.js
+++ Server/app.js
@@ -1,36 +1,63 @@
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-const csrfRoutes = require("./middleware/csrfMiddleWare")
-
+const csrfRoutes = require("./middleware/csrfMiddleWare");
+const morgan = require("morgan");
+const fs = require("fs");
 const path = require("path");
-const MONGO_URI =
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.wxdleee.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+const { v4: uuidv4 } = require('uuid');
+
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.wxdleee.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://next-carbon-git-main-sosenkkk.vercel.app", "https://next-carbon.vercel.app","https://next-carbon-al7l5k1lk-sosenkkk.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://next-carbon-git-main-sosenkkk.vercel.app",
+      "https://next-carbon.vercel.app",
+      "https://next-carbon-al7l5k1lk-sosenkkk.vercel.app",
+    ],
     methods: ["POST", "GET", "HEAD", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
+const logsDir = path.join(__dirname, "Logs");
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
+
+const logFileName = `${uuidv4()}.log`;
+const accessLogStream = fs.createWriteStream(
+  path.join(logsDir, logFileName),
+  { flags: 'a' }
+);
+
 app.use(cookieParser());
 app.use(bodyParser.json());
-// Use Morgan for logging requests
-app.use(morgan("dev"));
 
+app.use(
+  morgan(
+    (tokens, req, res) => {
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+      ].join(" ");
+    },
+    { stream: accessLogStream }
+  )
+);
 
 app.use(
   fileUpload({
@@ -40,14 +67,13 @@ app.use(
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use("/csrf", csrfRoutes)
+app.use("/csrf", csrfRoutes);
 
 app.use(authRoutes);
 
 app.use(adminRoutes);
 
 app.use(userRoutes);
-
 
 app.use((error, req, res, next) => {
   console.log(error);
