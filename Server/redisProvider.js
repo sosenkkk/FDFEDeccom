80a00a5db2325f899792699ce4e8b8ccfb861fa4
Tapesh-Vashisth <tapeshvashisth789@gmail.com>
2024-04-30 12:28:48 +0530

    resolved errors

--- Server/redisProvider.js
+++ Server/redisProvider.js
@@ -6,19 +6,18 @@ const redisClient = new Redis();
 async function getOrSetCache(key, ex, cb) {
     return new Promise(async (resolve, reject) => {
         redisClient.get(key, async (error, data) => {
-            // if (error) return reject(error)
+            if (error) return reject(error)
             if (data != null) {
                 return resolve(JSON.parse(data)); //convert to json
             }
-            const freshData = await cb()
-                .then((freshData)=>{
-                    redisClient.setex(key, ex, JSON.stringify(freshData)); //convert to string
-                    resolve(freshData);
-                })
-                .catch((err)=>{
-                    reject(err);
-                })
-        
+            cb()
+            .then((freshData)=>{
+                redisClient.setex(key, ex, JSON.stringify(freshData)); //convert to string
+                resolve(freshData);
+            })
+            .catch((err)=>{
+                reject(err);
+            })
         })
     })
 };
