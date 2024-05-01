7607f5bd6ab4d47f5943074f2343fc8c67ab6661
Tapesh-Vashisth <tapeshvashisth789@gmail.com>
2024-05-02 03:21:33 +0530

    jest

--- Server/app.js
+++ Server/app.js
@@ -4,6 +4,7 @@ const { app } = require("./server");
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster1.wxdleee.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 
+
 mongoose.connect(MONGO_URI).then(() => {
   app.listen(PORT);
   console.log("connected");
